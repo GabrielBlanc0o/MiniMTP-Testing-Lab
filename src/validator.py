87576594ce9562c00ec97f20f35759947ca149cf
GabrielBlanc0o <gtbvacation@outlook.es>
2026-03-04 12:18:19 -0500

    arreglo de condicionales #1 con ajuste del except

--- src/validator.py
+++ src/validator.py
@@ -1,21 +1,29 @@
+from time import *
+
 def validador(self,usuario,edad):
     self.usuario = str(usuario)
     self.edad = edad 
     
-    mayor_longitud_usuarioa = usuario > 12 < 5
-    
-    edad = int(edad)
-    mayor_lon_edad = edad >=18 < 99
+    #mayor_longitud_usuarioa = usuario > 12 < 5 mejor con len()
+    #mayor_lon_edad = edad >=18 < 99
+    if not (5 <= len(usuario) <= 12):
+        return "Usuarios de 5 - 12 caracteres solamente"
     
     try:     
-        if edad == mayor_lon_edad:
-            return f"edad fuera del rango {mayor_lon_edad}"
+        edad = int(edad)
+        if not (18 <= edad <= 99):
+            return f"edad fuera del rango {edad}"
         
-        if usuario  == mayor_longitud_usuarioa :
-            return ("Usuario de 5 - 12 caracteres solamente")
+        #if usuario  == mayor_longitud_usuarioa :
+            #return ("Usuario de 5 - 12 caracteres solamente")
+            # Solo try para la el rango de la edad y q sea solo de valorint
         
-    except edad and usuario is True:
-        return edad and usuario
+    except ValueError:
+        return "Error: la edad deber ser un numero"
+    
+    time(2)
+    return "Registro exitoso"
+    
     
           
     
